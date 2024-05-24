ec41cbc91e2be87ee49a7f014e52e4d31771bca4
Juan Rios <juanrios.ber@gmail.com>
2024-05-23 22:28:40 -0500

    about 404

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request)
+       //Url a la que se le dará acceso en las peticiones
+      ->header("Access-Control-Allow-Origin", "http://numerar.local/")
+      //Métodos que a los que se da acceso
+      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+      //Headers de la petición
+      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
+    }
+}
